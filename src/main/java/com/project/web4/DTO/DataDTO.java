5391a3c6f5b81961f0fe6266db3280b52a7e9178
Troynikova Veronika <troynikv@gmail.ru>
2022-12-20 23:04:01 +0300

    Change NotEmpty to NotNull

--- src/main/java/com/project/web4/DTO/DataDTO.java
+++ src/main/java/com/project/web4/DTO/DataDTO.java
@@ -3,15 +3,15 @@ package com.project.web4.DTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
 @Data
 @AllArgsConstructor
 public class DataDTO {
-    @NotEmpty(message = "x должен быть задан ")
+    @NotNull(message = "x должен быть задан ")
     private Double x;
-    @NotEmpty(message = "y должен быть задан")
+    @NotNull(message = "y должен быть задан")
     private Double y;
     @Positive(message = "r не может быть отрицательным")
     private Double r;
